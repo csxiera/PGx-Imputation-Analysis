aae8e6393099fb4aae01ad86f38e5c54656d1ff9
Courtney Lenz <83185663+csxiera@users.noreply.github.com>
2024-06-21 16:02:29 -0600

    Add files via upload

---
+++ src/star-mapping/batch-job-scripts/main_intersect.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --output=%j_intersect.out
+#SBATCH --error=%j_intersect.err
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=50G
+#SBATCH --time=12:00:00
+
+# Activate conda environment
+source ~/software/init-conda
+conda activate pgx
+
+# Move to directory with intersect script
+cd ~/mdsc508
+
+# Run vcf_intersect.sh script
+FOLDER="$1"
+./vcf_intersect2.sh "$FOLDER"
+
+# Remove unnecessary data from intersection results
+./cut_info.sh "$FOLDER"
+
+# Combine results into single file
+cd "$FOLDER"
+cat *_overlap.txt > overlap_all.txt
