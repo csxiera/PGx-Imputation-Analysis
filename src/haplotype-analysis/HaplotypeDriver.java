fa0f109894fca4ecda0d582b8c0ed05d5f4bcf58
Courtney Lenz <83185663+csxiera@users.noreply.github.com>
2024-06-21 17:10:04 -0600

    Update and rename haplotype_driver.java to HaplotypeDriver.java

---
+++ src/haplotype-analysis/HaplotypeDriver.java
@@ -0,0 +1,15 @@
+public class HaplotypeDriver {
+    
+    public static void main (String [] args){
+
+        String masterMatch = "../results/master_stars.csv",
+            all_haplotypes =  "../data/all_haplotypes.csv",
+            masterVars37 = "../data/pharmvar_variants_37.csv",
+            masterVars38 = "../data/pharmvar_variants_38.csv",
+            result = "../results/master_coverage_new.csv"
+        ;
+
+        HaplotypeCoverage hc = new HaplotypeCoverage();
+        hc.runAnalysis(masterMatch, all_haplotypes, masterVars37, masterVars38, result);
+    }
+}
